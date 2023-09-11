a59c1a8a1c9a0bbde181b6d42de91a5f01a54e8b
Jaiz183 <jaizepic@gmail.com>
2023-09-11 17:35:08 -0400

    Refactored to extract helper method.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,52 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        int j = 1;
+        while (j < 100) {
+            j = doFizzBuzz(j);
         }
+
+//        for (int i = 1; i < 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+    }
+
+    private static int doFizzBuzz(int j) {
+        boolean isDivisibleBy3 = j % 3 == 0;
+        boolean isDivisibleBy5 = j % 5 == 0;
+
+        // Print case by case.
+        if (isDivisibleBy3 && isDivisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (isDivisibleBy3) {
+            System.out.println("Fizz");
+        } else if (isDivisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(j);
+        }
+
+        j++;
+        return j;
     }
 }
